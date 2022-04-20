b67cc01f7ac7114eb04842d746cc7208affeb599
acf1204 <catharticaalamanda@gmail.com>
2022-04-20 08:33:55 +0700

    projek portofolio

---
+++ main.js
@@ -0,0 +1,15 @@
+/*
+The openNav function is used to open side navigation upto 60% width
+*/
+function openNav() {
+    document.getElementById("myNav").style.width = "60%";
+  }
+  /*
+  The openNav function is used to set side navigation upto 0% width
+  i.e. side navigation will not be visible
+  */
+  function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+  }
+  
+  
